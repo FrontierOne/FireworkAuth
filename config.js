b02892fd0ee816afc81f414d53f15346a04c2d46
achuthrajula <achuthrajula1999@gmail.com>
2019-10-06 20:35:25 +0530

    First Authentication Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretkey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/Auth',
+    'facebook': {
+        clientId: '365693977479144',
+        clientSecret: '7ec1ae01acfd9555a63764320771666d'
+    }
+}
