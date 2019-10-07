7eaa565571d865feee094c4c859aa29d3c717c71
achuthrajula <achuthrajula1999@gmail.com>
2019-10-07 10:34:25 +0530

    Mongodb Compass Setup

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretkey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/Auth',
+    'mongoUrl': 'mongodb+srv://alphaButtFucker:sullichiku@laniakea-1hblj.mongodb.net/students?retryWrites=true',
     'facebook': {
         clientId: '365693977479144',
         clientSecret: '7ec1ae01acfd9555a63764320771666d'
