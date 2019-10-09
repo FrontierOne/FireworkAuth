281d5f84f9f838bd7574cfdaa40f87bce7630895
achuthrajula <achuthrajula1999@gmail.com>
2019-10-09 20:45:07 +0530

    Removed Facebook signup

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 module.exports = {
     'secretkey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://alphaButtFucker:sullichiku@laniakea-1hblj.mongodb.net/students?retryWrites=true',
-    'facebook': {
-        clientId: '365693977479144',
-        clientSecret: '7ec1ae01acfd9555a63764320771666d'
-    }
+    'mongoUrl': 'mongodb+srv://alphaButtFucker:sullichiku@laniakea-1hblj.mongodb.net/students?retryWrites=true'
 }
